6982ac177c2fea96c80feaaae0c3278ea53779df
Wagner Jr <wagner.junior85@gmail.com>
2021-10-07 14:26:12 -0300

    Init

---
+++ src/main/java/com/fcamara/teste/FCamaraApplication.java
@@ -0,0 +1,13 @@
+package com.fcamara.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FCamaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FCamaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcamara/teste/FCamaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcamara.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FCamaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
